8607af5575e0fa38910df62f8bef6893f38adeb1
Dennis-Kiambi <denniskiriinya@gmail.com>
2020-10-08 10:26:45 +0300

    edit spacing on profile component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileService } from '../profile.service'
-
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
